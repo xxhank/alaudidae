6641982cd65e08a64453ef450c48bdb27db0677f
王超 <wangchao@tv365.net>
2018-06-25 20:09:06 +0800

    完善列表框

--- app/src/main/java/com/lrx/alaudidae/MainActivity.java
+++ app/src/main/java/com/lrx/alaudidae/MainActivity.java
@@ -1,13 +1,70 @@
 package com.lrx.alaudidae;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView               _recyclerView;
+    private RecyclerView.Adapter       _adapter;
+    private RecyclerView.LayoutManager _layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        _recyclerView = findViewById(R.id.my_recycler_view);
+        _recyclerView.setHasFixedSize(true);
+
+        _layoutManager = new LinearLayoutManager(this);
+        _recyclerView.setLayoutManager(_layoutManager);
+
+    }
+
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private String[] _dataset;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView _textView;
+
+            public ViewHolder(TextView itemView) {
+                super(itemView);
+                _textView = itemView;
+            }
+        }
+
+        MyAdapter(String[] dataset) {
+            _dataset = dataset;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            TextView view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.my_text_view, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder._textView.setText(_dataset[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return _dataset.length;
+        }
+
+
     }
 }
