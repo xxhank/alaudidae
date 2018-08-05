5a7a29aa51b5b5888b7b2b2359303e9d8011e6f5
王超9 <wangchao9@le.com>
2018-08-05 10:39:53 +0800

    引入jsbridge

--- app/src/main/java/com/lrx/alaudidae/MainActivity.java
+++ app/src/main/java/com/lrx/alaudidae/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lrx.alaudidae;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -9,63 +10,131 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.lzyzsd.jsbridge.BridgeHandler;
+import com.github.lzyzsd.jsbridge.BridgeWebView;
+import com.github.lzyzsd.jsbridge.CallBackFunction;
+import com.github.lzyzsd.jsbridge.DefaultHandler;
+import com.google.gson.GsonBuilder;
 import com.lrx.alaudidae.Model.BookResponse;
 import com.orhanobut.logger.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.Gson;
+
 import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-
-    private RecyclerView               _recyclerView;
-    private RecyclerView.Adapter       _adapter;
+    private RecyclerView _recyclerView;
+    private RecyclerView.Adapter _adapter;
     private RecyclerView.LayoutManager _layoutManager;
+
+    BridgeWebView _webView;
+    Button _parseButton;
 
     private Gson _gson;
     BookResponse _response;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            String[] files = getAssets().list("");
-
-            for (String file : files) {
-                Logger.d("file:" + file);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         _gson = new Gson();
         try {
-            InputStream books =  getAssets().open("books.json");
-            String bookJSONString =  IOUtils.toString(books, "UTF-8");
-            _response =  _gson.fromJson(bookJSONString, BookResponse.class);
-
+            InputStream books = getAssets().open("books2.json");
+            String bookJSONString = IOUtils.toString(books, "UTF-8");
+            _response = _gson.fromJson(bookJSONString, BookResponse.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        _recyclerView = findViewById(R.id.my_recycler_view);
+
+        _webView = findViewById(R.id.webView);
+        _webView.getSettings().setJavaScriptEnabled(true);
+        _webView.setWebContentsDebuggingEnabled(true);
+
+
+        //_webView.loadUrl("file:///android_asset/index.html");
+
+        _webView.setDefaultHandler(new DefaultHandler());
+
+        _webView.setWebChromeClient(new WebChromeClient() {
+            @SuppressWarnings("unused")
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType, String capture) {
+                this.openFileChooser(uploadMsg);
+            }
+
+            @SuppressWarnings("unused")
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType) {
+                this.openFileChooser(uploadMsg);
+            }
+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                //mUploadMessage = uploadMsg;
+                //pickFile();
+            }
+        });
+
+        _webView.loadUrl("file:///android_asset/index.html");
+
+        _webView.registerHandler("submitFromWeb", new BridgeHandler() {
+            @Override
+            public void handler(String data, CallBackFunction function) {
+                Logger.i("handler = submitFromWeb, data from web = " + data);
+                function.onCallBack("submitFromWeb exe, response data 中文 from Java");
+            }
+        });
+
+        _webView.callHandler("parse", "http://www.mailnovel.com/", new CallBackFunction() {
+            @Override
+            public void onCallBack(String data) {
+                Logger.d(data);
+            }
+        });
+
+        _parseButton = findViewById(R.id.parse_button);
+        _parseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _webView.callHandler("parse", "http://www.mailnovel.com/", new CallBackFunction() {
+                    @Override
+                    public void onCallBack(String data) {
+                        Logger.d(data);
+                    }
+                });
+            }
+        });
+        _recyclerView =  findViewById(R.id.my_recycler_view);
         _recyclerView.setHasFixedSize(true);
 
-        _layoutManager = new LinearLayoutManager(this);
+        _layoutManager = new
+
+                LinearLayoutManager(this);
         _recyclerView.setLayoutManager(_layoutManager);
 
-        _adapter = new MyAdapter(_response.books);
-        _recyclerView.setAdapter(_adapter);
-        _recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        _adapter = new
 
-        _recyclerView.addOnItemTouchListener(new RecyclerTouchListener(this, _recyclerView, new RecyclerTouchListener.ClickListener() {
+                MyAdapter(_response.books);
+        _recyclerView.setAdapter(_adapter);
+        _recyclerView.addItemDecoration(new
+
+                DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        _recyclerView.addOnItemTouchListener(new
+
+                RecyclerTouchListener(this, _recyclerView, new RecyclerTouchListener.ClickListener() {
             @Override
             public void onClick(View view, int position) {
                 Toast.makeText(MainActivity.this, "Single Click on position :" + position, Toast.LENGTH_SHORT).show();
@@ -81,19 +150,23 @@ public class MainActivity extends AppCompatActivity {
 
 
     public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-        private List<BookResponse.Book> _dataset;
+        private List<BookResponse.Book> books;
 
         public class ViewHolder extends RecyclerView.ViewHolder {
-            public TextView _textView;
+            public TextView titleView;
+            public TextView countView;
+            public TextView briefView;
 
             public ViewHolder(View itemView) {
                 super(itemView);
-                _textView = itemView.findViewById(R.id.book_title);
+                titleView = itemView.findViewById(R.id.book_title);
+                countView = itemView.findViewById(R.id.book_subscription_count);
+                briefView = itemView.findViewById(R.id.book_brief);
             }
         }
 
-        MyAdapter(List<BookResponse.Book> dataset) {
-            _dataset = dataset;
+        MyAdapter(List<BookResponse.Book> books) {
+            this.books = books;
         }
 
         @NonNull
@@ -105,13 +178,15 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-            BookResponse.Book book =  _dataset.get(position);
-            holder._textView.setText(book.title);
+            BookResponse.Book book = books.get(position);
+            holder.titleView.setText(book.title);
+            holder.countView.setText("" + book.subscriptionsCount);
+            holder.briefView.setText(book.brief);
         }
 
         @Override
         public int getItemCount() {
-            return _dataset.size();
+            return books.size();
         }
     }
 }
